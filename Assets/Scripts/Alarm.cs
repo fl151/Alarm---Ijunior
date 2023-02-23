b8e7099d22f2177fc0e313c4f57810509e6d30c2
fl151 <118074384+fl151@users.noreply.github.com>
2023-02-24 00:32:56 +0700

    Refacktoring

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -9,71 +9,48 @@ public class Alarm : MonoBehaviour
     [SerializeField] private float _recoveryRateSoundOff;
     [SerializeField] private float _maxSoundVolume;
 
-    private float _soundVolume;
     private readonly float _minSoundVolume = 0;
-    private Coroutine _alarmCoroutine;
+    private Coroutine _changeVolumeCoroutine;
 
     private void Awake()
     {
-        if(_maxSoundVolume >= 1)
-        {
-            _maxSoundVolume = 1;
-        }
-        if (_maxSoundVolume <=0)
-        {
-            _maxSoundVolume = 0;
-        }
+        Mathf.Clamp01(_maxSoundVolume);
 
-        _soundVolume = _minSoundVolume;
-    }
-
-    private void Update()
-    {
-        _audio.volume = _soundVolume;
-    }
-
-    private void SoundVolumeMoveTowards(float targetSoundVolume, float recoveryRate)
-    {
-        _soundVolume = Mathf.MoveTowards(_soundVolume, targetSoundVolume, recoveryRate * Time.deltaTime);
+        _audio.volume = _minSoundVolume;
     }
 
     public void StartAlarm()
     {
-        if(_alarmCoroutine != null)
-        {
-            StopCoroutine(_alarmCoroutine);
-        }
-
-        _alarmCoroutine = StartCoroutine(OnAlarm());
+        StartChangingVolume(_maxSoundVolume);
     }
 
     public void StopAlarm()
     {
-        if (_alarmCoroutine != null)
-        {
-            StopCoroutine(_alarmCoroutine);
-        }
-
-        _alarmCoroutine = StartCoroutine(OffAlarm());
+        StartChangingVolume(_minSoundVolume);
     }
 
-    private IEnumerator OnAlarm()
+    private void SoundVolumeMoveTowards(float targetSoundVolume, float recoveryRate)
     {
-        while (_maxSoundVolume > _soundVolume)
+        _audio.volume = Mathf.MoveTowards(_audio.volume, targetSoundVolume, recoveryRate * Time.deltaTime);
+    }
+
+    private void StartChangingVolume(float targetVolume)
+    {
+        if (_changeVolumeCoroutine != null)
         {
-            SoundVolumeMoveTowards(_maxSoundVolume, _recoveryRateSoundOn);
+            StopCoroutine(_changeVolumeCoroutine);
+        }
+
+        _changeVolumeCoroutine = StartCoroutine(ChangeAlarmVolume(targetVolume));
+    }
+
+    private IEnumerator ChangeAlarmVolume(float targetVolume)
+    {
+        while (targetVolume != _audio.volume)
+        {
+            SoundVolumeMoveTowards(targetVolume, _recoveryRateSoundOn);
 
             yield return null;
         } 
-    }
-
-    private IEnumerator OffAlarm()
-    {
-        while (_minSoundVolume < _soundVolume)
-        {
-            SoundVolumeMoveTowards(_minSoundVolume, _recoveryRateSoundOff);
-
-            yield return null;
-        }
     }
 }
